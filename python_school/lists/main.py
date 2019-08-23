62ff987da686bbfd32e6feaae1a1b929abbcae66
Antoine Muller <nemak121@gmail.com>
2019-08-23 08:29:00 +0200

    add max_element function

--- python_school/lists/main.py
+++ python_school/lists/main.py
@@ -22,10 +22,22 @@ def find_element(items, predicate):
     
     return found_item
 
+def max_element(items):
+    if items == None or len(items) == 0:
+        return None
+    
+    max_element = items[0]
+
+    for item in items:
+        if(item > max_element): 
+            max_element = item
+    
+    return max_element
 
 
 if __name__ == "__main__":
-    items = [21, 1, 4, 78, 56, 18, 93, 876]
+    items = [21, 1, 4, 78, 2099, 56, 18, 93, 876]
 
     print(f"Simple Element {simple_find_element(items, 56)}")
     print(f"Find Element {find_element(items, lambda x : x == 56)}")
+    print(f"Max Element {max_element(items)}")
