2436aa1cc648f877c8e2f048f996e04b32ea1d1a
Antoine Muller <nemak121@gmail.com>
2019-09-04 10:33:46 +0200

    add exif geolocation data

--- python_school/pictures/pictures.py
+++ python_school/pictures/pictures.py
@@ -1,4 +1,5 @@
 from PIL import Image
+from PIL.ExifTags import TAGS, GPSTAGS
 
 def rgb_pixel_to_grey(r, g, b):
     avg = (r+g+b)/3
@@ -32,6 +33,51 @@ def image_to_negative(base_image):
 
     return negative_image
 
+# EXIF
+def get_exif(img):
+    return img._getexif()
+
+def get_labeled_geotagging(exif_geotagging_value):
+    labeled_geotagging = {}
+
+    for (key, tag) in GPSTAGS.items():
+        if key in exif_geotagging_value:
+            labeled_geotagging[tag] = exif_geotagging_value[key]
+    
+    return labeled_geotagging
+
+def get_labeled_exif(img):
+    raw_exif = get_exif(img)
+    labeled_exif = {}
+    for (key, val) in raw_exif.items():
+        tag = TAGS.get(key)
+        if tag == 'GPSInfo':
+            labeled_exif[tag] = get_labeled_geotagging(val)
+        else:
+            labeled_exif[tag] = val
+
+    return labeled_exif
+
+# Get gps data from geotagging
+def get_decimal_from_dms(dms, ref):
+
+    degrees = dms[0][0] / dms[0][1]
+    minutes = dms[1][0] / dms[1][1] / 60.0
+    seconds = dms[2][0] / dms[2][1] / 3600.0
+
+    if ref in ['S', 'W']:
+        degrees = -degrees
+        minutes = -minutes
+        seconds = -seconds
+
+    return round(degrees + minutes + seconds, 5)
+
+def get_coordinates(geotags):
+    lat = get_decimal_from_dms(geotags['GPSLatitude'], geotags['GPSLatitudeRef'])
+
+    lon = get_decimal_from_dms(geotags['GPSLongitude'], geotags['GPSLongitudeRef'])
+
+    return (lat,lon)
 
 if __name__ == "__main__":
     print("Hello pictures :D")
@@ -40,6 +86,12 @@ if __name__ == "__main__":
     res_images_path = 'res_images'
 
     street_art_image = Image.open(f'{images_path}/street_art.jpeg')
+    mario_art_image = Image.open(f'{images_path}/mario_art.jpg')
+
+    # Exif
+    labeled_exif = get_labeled_exif(mario_art_image)
+    print(f'Exif : {labeled_exif}')
+    print(f'Exif coordinate (lat, lon) : {get_coordinates(labeled_exif["GPSInfo"])}')
 
     # Negative
     street_art_negative_image = image_to_negative(street_art_image)
