e069dab5df2d6ade618f16138c5ed1d5043bdf3e
Antoine Muller <nemak121@gmail.com>
2019-10-07 14:44:28 +0200

    fix bug with boyer_moore horspool

--- python_school/string_search/boyer_moore.py
+++ python_school/string_search/boyer_moore.py
@@ -4,13 +4,13 @@ def offset_memo(text):
     for i in range(text_length - 1):
         current_char = text[i]
         memo[current_char] = text_length - i - 1
+    print(f"Memo : {memo}")
     return memo
 
 def boyer_moore_horspool(text, search_text):
     memo = offset_memo(search_text)
     text_offset = 0
-
-    while text_offset < (len(text) - len(search_text)):
+    while text_offset <= (len(text) - len(search_text)):
 
         i = len(search_text) - 1
 
@@ -33,3 +33,7 @@ if __name__ == "__main__":
     text = "Bonjour josianne, comment ça va ?"
     search_text = "comment"
     print(f"boyer_moore_horspool({text}, {search_text}) {boyer_moore_horspool(text, search_text)}")
+
+    text_b = "AACBC"
+    search_text_b = "CBC"
+    print(f"boyer_moore_horspool({text_b}, {search_text_b}) {boyer_moore_horspool(text_b, search_text_b)}")
