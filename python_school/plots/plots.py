c5328715de6a00a2eaabce6c67944d779389714f
Antoine Muller <nemak121@gmail.com>
2019-10-28 12:10:57 +0100

    add examples with plot

--- python_school/plots/plots.py
+++ python_school/plots/plots.py
@@ -1,8 +1,22 @@
 import matplotlib.pyplot as plt
 
-if __name__ == "__main__":
-    print("Hello plot")
+def make_basic_plot():
     plt.plot([1, 2, 3, 4])
     plt.ylabel('some numbers')
-    plt.savefig('foo.png')
+    plt.savefig('basic_plot.png')
+    plt.clf()
+
+def make_scatter_plot():
+    xs = [1, 2, 3, 4]
+    ys = [1, 2, 3, 4]
+    # fig, ax = plt.subplots()
+    plt.scatter(xs, ys, color='g', label="foo")
+    plt.legend()
+    plt.savefig('scatter_plot.png')
+    plt.clf()
+
+if __name__ == "__main__":
+    print("Hello plot")
+    make_basic_plot()
+    make_scatter_plot()
     # plt.show()
