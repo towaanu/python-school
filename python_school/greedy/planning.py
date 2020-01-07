d789d5ff26ce71f6285f9306857b88fdaba5db3b
Antoine Muller <nemak121@gmail.com>
2020-01-07 16:00:54 +0100

    add greedy planning

---
+++ python_school/greedy/planning.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+import random
+
+def display_planning(activities, file_name="planning.png"):
+    plt.ylabel("Activities")
+    plt.xlabel("Time")
+
+    for i in range(len(activities)):
+        (begin_hour, end_hour) = activities[i]
+        plt.barh(i, end_hour - begin_hour, left=[begin_hour], height=0.4)
+    
+    plt.savefig(file_name)
+    plt.clf()
+
+def generate_planning(activities):
+    sorted_activities = sorted(activities, key= lambda a : a[1])
+    last_activity = None
+    planning = []
+    for activity in sorted_activities:
+        (begin_hour, end_hour) = activity
+
+        if last_activity is None or begin_hour >= last_activity[1]:
+            planning.append(activity)
+            last_activity = activity
+
+    return planning
+
+def generate_random_activities(nb_activities):
+    random.seed()
+
+    rand_activities = []
+    for i in range(nb_activities):
+        begin_hour = random.randrange(0, 23)
+        end_hour = random.randrange(begin_hour + 1, 24)
+
+        rand_activities.append((begin_hour, end_hour))
+    
+    return rand_activities
+
+
+if __name__ == "__main__":
+    print("Hello planning :D")
+
+    # activities = [(1, 5), (2, 5), (1, 2), (3, 4)]
+    activities = generate_random_activities(10)
+    display_planning(activities, "activities.png")
+
+    res_planning = generate_planning(activities)
+    display_planning(res_planning, "planning.png")
+    print(f"Res planning {res_planning}")
