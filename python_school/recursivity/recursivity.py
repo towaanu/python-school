cbbfa16bda572087f12529bd1d788ea301f4f062
Antoine Muller <niantoinesan@gmail.com>
2020-02-18 16:00:45 +0100

    add recurivity example

--- python_school/recursivity/recursivity.py
+++ python_school/recursivity/recursivity.py
@@ -7,7 +7,26 @@ def a(n):
     
     print(f"a is done with {n}")
 
+def factorial(n):
+    if n <= 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+def factorial_with_print(n):
+    print(f"[Factorial enter] {n}")
+    if n <= 1:
+        print(f"[Factorial return] {n}")
+        return 1
+    else:
+        n_minus_one = factorial_with_print(n - 1)
+        print(f"[Factorial return] {n}")
+        return n * n_minus_one
+    
+
 if __name__ == "__main__":
 
     print("Graph :D")
     a(5)
+    print(f"facto(5) : {factorial(5)}")
+    print(f"facto(6) : {factorial_with_print(6)}")
